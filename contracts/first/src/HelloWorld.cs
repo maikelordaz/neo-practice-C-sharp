5161ade73a727b3c3b060aa9e57bafe0f70e47b1
Maikel Ordaz <maikelordaz@gmail.com>
2023-06-03 22:40:10 -0400

    First commit

---
+++ contracts/first/src/HelloWorld.cs
@@ -0,0 +1,14 @@
+using Neo;
+using Neo.SmartContract;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+using System;
+
+namespace Helloworld
+{
+    public class Helloworld : SmartContract
+    {
+
+    }
+}
---
+++ contracts/first/src/firstContract.cs
@@ -0,0 +1,30 @@
+﻿// using => imports
+using System;
+using System.ComponentModel;
+using System.Numerics;
+
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace First
+{
+    
+    public class FirstContract : SmartContract
+    {
+        // Las funciones se llaman methods
+        public static void AddData(object data)
+        {
+            // Estoy guardando en Storage en la key name el nombre maikel
+            // Lo guardo en el CurrentContext de storage
+            Storage.Put(Storage.CurrentContext, "Name", "Maikel");
+        }
+
+        public static string getData()
+        {
+            return Storage.Get(Storage.CurrentContext, "Name");
+        }
+    }
+}
